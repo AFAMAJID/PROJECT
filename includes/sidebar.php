ced16c2b134d8f9122560ff003a34a66e428f82d
AFAMAJID <safatsawo@gmail.com>
2022-05-18 13:32:46 +0000

    transformed to php

---
+++ includes/sidebar.php
@@ -0,0 +1,85 @@
+<aside class="sidebar trans-0-4">
+		<!-- Button Hide sidebar -->
+		<button class="btn-hide-sidebar ti-close color0-hov trans-0-4"></button>
+
+		<!-- - -->
+		<ul class="menu-sidebar p-t-95 p-b-70">
+			<li class="t-center m-b-13">
+				<a href="index.php" class="txt19">Home</a>
+			</li>
+
+			<li class="t-center m-b-13">
+				<a href="menu.php" class="txt19">Menu</a>
+			</li>
+
+			<li class="t-center m-b-13">
+				<a href="gallery.php" class="txt19">Gallery</a>
+			</li>
+
+			<li class="t-center m-b-13">
+				<a href="about.php" class="txt19">About</a>
+			</li>
+
+			<li class="t-center m-b-13">
+				<a href="blog.php" class="txt19">Blog</a>
+			</li>
+
+			<li class="t-center m-b-33">
+				<a href="contact.php" class="txt19">Contact</a>
+			</li>
+
+			<li class="t-center">
+				<!-- Button3 -->
+				<a href="reservation.php" class="btn3 flex-c-m size13 txt11 trans-0-4 m-l-r-auto">
+					Reservation
+				</a>
+			</li>
+		</ul>
+
+		<!-- - -->
+		<div class="gallery-sidebar t-center p-l-60 p-r-60 p-b-40">
+			<!-- - -->
+			<h4 class="txt20 m-b-33">
+				Gallery
+			</h4>
+
+			<!-- Gallery -->
+			<div class="wrap-gallery-sidebar flex-w">
+				<a class="item-gallery-sidebar wrap-pic-w" href="images/refem.jpg" data-lightbox="gallery-footer">
+					<img src="images/refem.jpg" alt="GALLERY">
+				</a>
+
+				<a class="item-gallery-sidebar wrap-pic-w" href="images/IMG_8521.jpg" data-lightbox="gallery-footer">
+					<img src="images/IMG_8521.jpg" alt="GALLERY">
+				</a>
+
+				<a class="item-gallery-sidebar wrap-pic-w" href="images/IMG_8567.jpg" data-lightbox="gallery-footer">
+					<img src="images/IMG_8567.jpg" alt="GALLERY">
+				</a>
+
+				<a class="item-gallery-sidebar wrap-pic-w" href="images/IMG_8550.jpg" data-lightbox="gallery-footer">
+					<img src="images/IMG_8550.jpg" alt="GALLERY">
+				</a>
+
+				<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-06.jpg" data-lightbox="gallery-footer">
+					<img src="images/IMG_8576.jpg" alt="GALLERY">
+				</a>
+
+				<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-07.jpg" data-lightbox="gallery-footer">
+					<img src="images/IMG_8805.jpg" alt="GALLERY">
+				</a>
+
+				<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-09.jpg" data-lightbox="gallery-footer">
+					<img src="images/IMG_8489.jpg" alt="GALLERY">
+				</a>
+
+				<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-10.jpg" data-lightbox="gallery-footer">
+					<img src="images/IMG_8521.jpg" alt="GALLERY">
+				</a>
+
+				<a class="item-gallery-sidebar wrap-pic-w" href="images/photo-gallery-11.jpg" data-lightbox="gallery-footer">
+					<img src="images/IMG_8567.jpg" alt="GALLERY">
+				</a>
+			</div>
+		</div>
+	</aside>
